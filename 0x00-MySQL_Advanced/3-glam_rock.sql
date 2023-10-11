f4f6cda6307bbf1f0e47029c1e2c0c8aa83d01f2
EBENEZER MARADESA <77102601+maradeben@users.noreply.github.com>
2023-10-11 23:05:39 +0100

    Update 3-glam_rock.sql

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,7 +1,7 @@
 -- list all bands with Glam rock as main style
 -- ranked by longevity
 
-SELECT band_name AS band_name, IFNULL(split, 2020) - IFNULL(formed, 0) AS lifespan 
- FROM metal_bands 
- WHERE style LIKE '%Glam rock%' 
- ORDER BY lifespan DESC;
+SELECT band_name AS band_name, (2020 - formed) AS lifespan 
+   FROM metal_bands 
+   WHERE style LIKE '%Glam rock%' 
+   ORDER BY lifespan DESC;
