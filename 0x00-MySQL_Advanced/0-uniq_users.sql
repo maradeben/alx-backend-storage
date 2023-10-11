66362b450f61a58357b665851be89fceefe9bf09
Eben <maradebenezer@gmail.com>
2023-10-11 15:51:29 +0100

    completed task 0

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,10 @@
+-- create TABLE users with:
+-- id, email and name
+-- scripts should run if table not exists
+-- can be executed on any database
+
+CREATE TABLE IF NOT EXISTS users (
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	email VARCHAR(255) NOT NULL UNIQUE,
+	name VARCHAR(255)
+	)
---
+++ 0x00-MySQL_Advanced/README.md
@@ -0,0 +1,8 @@
+# 0x00. MySQL advanced
+
+### Objectives
+- Create tables with constraints
+- Optimize queries by adding indexes
+- Implementing sotred procedures and functions
+- Implementing views in MySQL
+- Implementing triggers in MySQL
